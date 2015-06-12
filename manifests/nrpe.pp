99c965520cee7224deb64929a0d31565fe3ed47f
Adam Breaux <breauxa@email.chop.edu>
2015-06-12 13:57:14 -0400

    saving some time

--- manifests/nrpe.pp
+++ manifests/nrpe.pp
@@ -18,22 +18,26 @@ class profile::nrpe {
     crit_value => $profile_nrpe_disks['crit_value'],
   }
 
-  $profile_nrpe_httpd = hiera_hash('profile::nrpe_httpd',{})
+  $profile_nrpe_httpd = hiera('profile::nrpe_httpd',{})
   create_resources('nrpe::httpd',$profile_nrpe_httpd)
 
-  $profile_nrpe_java = hiera_hash('profile::nrpe_java',{})
+  $profile_nrpe_java = hiera('profile::nrpe_java',{})
   create_resources('nrpe::java',$profile_nrpe_java)
 
-  $profile_nrpe_ldap = hiera_hash('profile::nrpe_ldap',{})
+  $profile_nrpe_ldap = hiera('profile::nrpe_ldap',{})
   create_resources('nrpe::ldap',$profile_nrpe_ldap)
 
-  $profile_nrpe_load = hiera_hash('profile::nrpe_load',{})
-  create_resources('nrpe::load',$profile_nrpe_load)
+  $profile_nrpe_load = hiera('profile::nrpe_load',{})
+  
+  nrpe::load { 'default':
+    warn_value => $profile_nrpe_load['warn_value'],
+    crit_value => $profile_nrpe_load['crit_value'],
+  }
 
-  $profile_nrpe_mysql = hiera_hash('profile::nrpe_mysql',{})
+  $profile_nrpe_mysql = hiera('profile::nrpe_mysql',{})
   create_resources('nrpe::mysql',$profile_nrpe_mysql)
 
-  $profile_nrpe_procs = hiera_hash('profile::nrpe_procs',{})
+  $profile_nrpe_procs = hiera('profile::nrpe_procs',{})
   create_resources('nrpe::procs',$profile_nrpe_procs)
 
   $profile_nrpe_service = hiera('profile::nrpe_service',{})
@@ -43,10 +47,10 @@ class profile::nrpe {
     enable => $profile_nrpe_service['enable'],
   }
 
-  $profile_nrpe_users = hiera_hash('profile::nrpe_users',{})
+  $profile_nrpe_users = hiera('profile::nrpe_users',{})
   create_resources('nrpe::users',$profile_nrpe_users)
 
-  $profile_nrpe_varnish = hiera_hash('profile::nrpe_varnish',{})
+  $profile_nrpe_varnish = hiera('profile::nrpe_varnish',{})
   create_resources('nrpe::varnish',$profile_nrpe_varnish)
 
 }
