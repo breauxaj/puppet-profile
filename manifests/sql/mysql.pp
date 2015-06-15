4274f7b50dc8060355d75f9f99b9aabc0ed53ef6
Adam Breaux <breauxa@email.chop.edu>
2015-06-15 08:21:11 -0400

    sql servers

---
+++ manifests/sql/mysql.pp
@@ -0,0 +1,11 @@
+class profile::sql::mysql {
+  include ::nrpe
+
+  $profile_mon_mysql = hiera('profile::mon::mysql',undef)
+
+  nrpe::mysql { 'default':
+    warn_value => $profile_mon_mysql['warn_value'],
+    crit_value => $profile_mon_mysql['crit_value'],
+  }
+
+}
