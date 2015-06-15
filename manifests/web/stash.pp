95234db11e5342fe1843f63d96941af7cd43c393
Adam Breaux <breauxa@email.chop.edu>
2015-06-15 08:24:17 -0400

    copy of tomcat profile

---
+++ manifests/web/stash.pp
@@ -0,0 +1,11 @@
+class profile::web::stash {
+  include ::nrpe
+
+  $profile_mon_java = hiera('profile::mon::java',{})
+
+  nrpe::java { 'default':
+    warn_value => $profile_mon_java['warn_value'],
+    crit_value => $profile_mon_java['crit_value'],
+  }
+
+}
