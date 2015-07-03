6b567982977808217f1259483d1802f8ad39a184
Adam Breaux <breauxaj@gmail.com>
2015-07-03 19:43:53 -0400

    wip

--- manifests/web/tomcat.pp
+++ manifests/web/tomcat.pp
@@ -1,4 +1,13 @@
 class profile::web::tomcat {
   hiera_include('profile::web::tomcat', '')
 
+  $tomcat_config = hiera('tomcat::config',{})
+
+  tomcat::config { 'default':
+    runas         = $tomcat_config['runas'],
+    catalina_home = $tomcat_config['catalina_home'],
+    catalina_base = $tomcat_config['catalina_base'],
+    java_opts     = $tomcat_config['java_opts'],
+  }
+
 }
