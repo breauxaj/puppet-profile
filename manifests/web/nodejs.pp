5848bc09b47262a479f2401212847e8cec67c7dd
Adam Breaux <breauxaj@gmail.com>
2015-07-03 19:26:05 -0400

    wrapper

--- manifests/web/nodejs.pp
+++ manifests/web/nodejs.pp
@@ -1,4 +1,12 @@
 class profile::web::nodejs {
   hiera_include('profile::web::nodejs', '')
 
+  $nodejs_instance = hiera('nodejs::instance',{})
+  
+  nodejs::instance { 'default':
+    user  => $nodejs_instance['user'],
+    group => $nodejs_instance['group'],
+    path  => $nodejs_instance['path'],
+  }
+
 }
