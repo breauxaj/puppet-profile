718b211f2326df54c032338ad70f34bac4ad7766
Adam Breaux <breauxaj@gmail.com>
2015-07-03 19:35:37 -0400

    complete

--- manifests/web/nodejs.pp
+++ manifests/web/nodejs.pp
@@ -2,11 +2,19 @@ class profile::web::nodejs {
   hiera_include('profile::web::nodejs', '')
 
   $nodejs_instance = hiera('nodejs::instance',{})
-  
-  nodejs::instance { 'default':
-    user  => $nodejs_instance['user'],
-    group => $nodejs_instance['group'],
-    path  => $nodejs_instance['path'],
+  create_resources('nodejs::instance',$nodejs_instance)
+
+  $nodejs_service = hiera('nodejs::service',{})
+  create_resources('nodejs::service',$nodejs_service)
+
+  $supervisor_hook = hiera('supervisor::hook',{})
+  create_resources('supervisor::hook',$supervisor_hook)
+
+  $supervisor_service = hiera('supervisor::service',{})
+
+  supervisor::service { 'default':
+    ensure => $supervisor_service['ensure'],
+    enable => $supervisor_service['enable'],
   }
 
 }
