82b13031a1fe5eebc67636fdb20e09396f661906
Adam Breaux <breauxaj@gmail.com>
2015-07-03 18:44:24 -0400

    finished

--- manifests/sql/mongodb.pp
+++ manifests/sql/mongodb.pp
@@ -1,4 +1,21 @@
 class profile::sql::mongodb {
   hiera_include('profile::sql::mongodb', '')
-  
+
+  $mongodb_config = hiera('mongodb::config',{})
+
+  mongodb::config (
+    bind_ip => $mongodb_config['bind_ip'],
+    port    => $mongodb_config['port'],
+    dbpath  => $mongodb_config['dbpath'],
+    auth    => $mongodb_config['auth'],
+    verbose => $mongodb_config['verbose'],
+  }
+
+  $mongodb_service = hiera('mongodb::service',{})
+
+  mongodb::service { 'default':
+    ensure => $mongodb_service['ensure'],
+    enable => $mongodb_service['enable'],
+  }
+
 }
