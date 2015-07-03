4af9eba6ba2d342589665ea0c437c8faeded2ca1
Adam Breaux <breauxaj@gmail.com>
2015-07-03 19:41:02 -0400

    completed

--- manifests/web/stash.pp
+++ manifests/web/stash.pp
@@ -1,4 +1,18 @@
 class profile::web::stash {
   hiera_include('profile::web::stash', '')
 
+  $stash_config = hiera('stash::config',{})
+
+  stash::config { 'default':
+    runas         => $stash_config['runas'],
+    catalina_home => $stash_config['catalina_home'],
+  }
+
+  $stash_service = hiera('stash::service',{})
+
+  stash::service { 'default':
+    ensure => $stash_service['ensure'],
+    enable => $stash_service['enable'],
+  }
+
 }
