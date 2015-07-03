500e4080014ed61879908634bae39ca02310fccd
Adam Breaux <breauxaj@gmail.com>
2015-07-03 18:53:02 -0400

    remove it

--- manifests/web/apache.pp
+++ manifests/web/apache.pp
@@ -1,13 +1,4 @@
 class profile::web::apache {
-  include ::nrpe
-
-  $profile_mon_httpd = hiera('profile::mon::httpd',{})
-  
-  nrpe::httpd { 'default':
-    warn_value => $profile_mon_httpd['warn_value'],
-    crit_value => $profile_mon_httpd['crit_value'],
-  }
-
   hiera_include('profile::web::apache', '')
 
   $apache_config = hiera('apache::config',{})
