5618d4185648d9d18d4bab38ca325f3d6c5832e1
Adam Breaux <breauxa@email.chop.edu>
2015-07-27 09:47:27 -0400

    appears to be the only wrapper needed

--- manifests/mon/nagios.pp
+++ manifests/mon/nagios.pp
@@ -1,4 +1,11 @@
 class profile::mon::nagios {
   hiera_include('profile::mon::nagios', '')
 
+  $nagios_service = hiera('nagios::service',{})
+
+  nagios::service { 'default':
+    ensure => $nagios_service['ensure'],
+    enable => $nagios_service['enable'],
+  }
+
 }
