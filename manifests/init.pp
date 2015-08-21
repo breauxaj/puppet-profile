b9e5195ae51426c59212da013489588abd8f419d
Adam Breaux <breauxaj@gmail.com>
2015-08-21 13:27:27 -0400

    they have to be hooked in order to articulate

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,9 @@
 class profile {
   include profile::mon::nrpe
   include profile::ops::haveged
+  include profile::ops::postfix
+  include profile::ops::rsyslog
+  include profile::ops::ssh
+  include profile::ops::sysctl
 
 }
