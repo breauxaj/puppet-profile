c87f97079e7c1de10daf520f67d4801638d3a4b7
Adam Breaux <breauxaj@gmail.com>
2015-08-21 15:13:01 -0400

    dont do it there

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,5 @@
 class profile {
   include profile::mon::nrpe
   include profile::ops::haveged
-  include profile::ops::postfix
-  include profile::ops::rsyslog
-  include profile::ops::ssh
-  include profile::ops::sysctl
 
 }
